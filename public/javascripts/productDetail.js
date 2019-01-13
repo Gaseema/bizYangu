b6ba04c563d96d5dbe4f2120981fe9879a304b5e
gaseema.n <gaseema@bitsoko.io>
2019-01-13 23:42:56 +0300

    added checkout modal, login modal

--- public/javascripts/productDetail.js
+++ public/javascripts/productDetail.js
@@ -1,4 +1,16 @@
-$(document).on('click touchstart', '.makeOrder', function(e){
-    console.log(getUrlParams('bizId'))
-    console.log(getUrlParams('prodId'))
+$(document).on('click touchstart', '.makeOrder', function(e) {
+    if (document.getElementById("loggedIn") == null) {
+        $('#loginModal').modal('show');
+    } else {
+        var itemName = $('.purchaseItemName').html();
+        var itemNo = $('.purchaseItemNo').html();
+        var itemPrice = $('.item_price').html().replace(/\D/g,'');
+        var itemTotalPrice = itemPrice * itemNo;
+        $('.purchaseListItem').html(itemName);
+        $('.purchaseListQty').html(itemNo);
+        $('.purchaseListPrice').html(itemTotalPrice);
+        $('#confirmModal').modal('show');
+    }
+    var bizId = getUrlParams('bizId');
+    var prodId = getUrlParams('prodId');
 })
